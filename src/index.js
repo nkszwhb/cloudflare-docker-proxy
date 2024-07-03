f763275212c75dc50a66aecd35d270a32f0789b0
haibo_wang <haibo_wang@trip.com>
2024-07-03 10:59:58 +0800

    domain modify

--- src/index.js
+++ src/index.js
@@ -7,17 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.hideonheart.top": dockerHub,
+  "quay.hideonheart.top": "https://quay.io",
+  "gcr.hideonheart.top": "https://gcr.io",
+  "k8s-gcr.hideonheart.top": "https://k8s.gcr.io",
+  "k8s.hideonheart.top": "https://registry.k8s.io",
+  "ghcr.hideonheart.top": "https://ghcr.io",
+  "cloudsmith.hideonheart.top": "https://docker.cloudsmith.io",
+  "ecr.hideonheart.top": "https://public.ecr.aws"
 };
 
 function routeByHosts(host) {
